006b5ae5838ba976f6a2635d7ab16323a35da414
Milan <mc@radiopiloten.de>
2022-04-22 13:35:26 +0200

    template/*: add CORS

--- website/agenda/templates/series_names.js
+++ website/agenda/templates/series_names.js
@@ -1,5 +1,6 @@
 <TMPL_IF use_client_cache>Cache-Control: max-age=3600, must-revalidate
 </TMPL_IF>Content-type:application/json; charset=UTF-8;
+Access-Control-Allow-Origin: *
 
 [
 <TMPL_LOOP name=projects><TMPL_LOOP name=series_names>{"id":"<TMPL_VAR series_name>", "label":"<TMPL_VAR series_name>", "value":"<TMPL_VAR series_name>"} <TMPL_UNLESS last>,</TMPL_UNLESS>
