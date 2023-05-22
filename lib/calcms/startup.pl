4cba61bf0636397a15af4df2ffe4415668e83868
Milan <mc@radiopiloten.de>
2023-05-22 23:51:05 +0200

    startup.pl: use ErrorLog file from Apache2 config
    requires Apache2::Log from libapache2-mod-perl2

--- lib/calcms/startup.pl
+++ lib/calcms/startup.pl
@@ -1,6 +1,12 @@
 use lib qw(/home/radio/calcms/calcms/);
 use lib qw(/home/calcms/lib/calcms/);
 
+use Apache2::Log;
+local *CORE::GLOBAL::warn = \&Apache2::ServerRec::warn;
+local $SIG{__WARN__} = \&Apache2::ServerRec::warn;
+# ^ use ErrorLog file set at Apache2 configuration
+#   see https://perl.apache.org/docs/2.0/api/Apache2/Log.html for details
+
 use Data::Dumper;
 #use Apache::DBI();
 use Time::Local();
