b1973a3aa0d50e33c2ac81c122fc5b88021362f4
Milan <mc@radiopiloten.de>
2024-08-06 22:58:56 +0200

    config.pm: add parse_size

--- lib/calcms/config.pm
+++ lib/calcms/config.pm
@@ -5,7 +5,7 @@ use warnings;
 no warnings 'redefine';
 
 use FindBin();
-our @EXPORT_OK = qw(get set);
+our @EXPORT_OK = qw(get set parse_size);
 
 my $config = undef;
 sub get($) {
@@ -17,6 +17,13 @@ sub getFromScriptLocation() {
     FindBin::again();
     my $configFile = $FindBin::Bin . '/config/config.cgi';
     return config::get($configFile);
+}
+
+sub parse_size {
+    my ($size) = @_;
+    my %units = (B => 1, KB => 1024, MB => 1024**2, GB => 1024**3);
+    $size =~ /^(\d+)\s*([kmgtp]?b)$/i or die "Invalid size format: $size\n";
+    return $1 * $units{uc($2)};
 }
 
 sub read_config {
