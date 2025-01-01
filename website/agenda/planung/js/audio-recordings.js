0b538e6f221c4545165eab4553c821a575bbb53e
Milan <mc@radiopiloten.de>
2025-01-01 19:35:52 +0100

    audio-upload: upload progress in title

--- website/agenda/planung/js/audio-recordings.js
+++ website/agenda/planung/js/audio-recordings.js
@@ -104,6 +104,7 @@ function uploadFile(uploadButton){
                         $('#progress_todo').css("width", (100-perc)+ "%")
                         $('#progress_done').html(loaded + " MB");
                         $('#progress_todo').html(remaining+" left");
+                        document.title = perc + "%" + $('#title').text();
 
                         let content = total + " MB<br>";
                          $('#progressLabel').html(content);
