230a9284267e4a97b2bccd035f48cfba913edc6c
Milan <mc@radiopiloten.de>
2026-06-07 17:21:52 +0200

    split calendar.js for list and table

---
+++ website/agenda/planung/js/calendar-tools.js
@@ -0,0 +1,97 @@
+if (window.namespace_calendar_tools_js) throw "stop"; window.namespace_calendar_tools_js = true;
+"use strict";
+
+function setColors() {
+    var elem = $('.schedule').get(0);
+    if (!elem)  return;
+    var col1 = window.getComputedStyle(elem).backgroundColor;
+    var col2 = col1.replace('rgb', 'rgba').replace(')', ', 0.4)');
+    var gradient = `repeating-linear-gradient(to bottom, ${col1}, ${col1} 1px, ${col2} 1px, ${col2} 2px)`;
+    $('.schedule').css('background', gradient);
+}
+
+function setup_actions() {
+    var base = $('#event_list, #calendar').first();
+
+    base.off().on("mousedown", ".event", function(e) {
+        handleEvent($(this).attr("id"), e);
+    });
+    base.on("click", ".event.no_series", function() {
+        handleUnassignedEvent($(this).attr("id"));
+    });
+    base.on("mousedown", ".schedule", function(e) {
+        handleSchedule($(this).attr("id"), $(this).attr("start"), e);
+    });
+    base.on("click", ".grid", function() {
+        handleGrid($(this).attr("id"));
+    });
+    base.on("mousedown", ".work", function(e) {
+        handleWorktime($(this).attr("id"), e);
+    });
+}
+
+function handleEvent(id, event) {
+    const [, project_id, studio_id, series_id, event_id] = id.split('_');
+    if (project_id < 0 || studio_id < 0 || series_id < 0 || event_id < 0) {
+        return;
+    }
+    var params = new URLSearchParams({ project_id, studio_id, series_id, event_id, action: 'edit' });
+    var url = 'broadcast.cgi?' + params.toString();
+    if (event.which == 1) {
+        loadUrl(url);
+    }
+    if (event.which == 2) {
+        openNewTab(url);
+    }
+}
+
+function handleUnassignedEvent(id) {
+    const [, project_id, studio_id, series_id, event_id] = id.split('_');
+    if (checkStudio() == 0 || project_id < 0 || event_id < 0) {
+        return;
+    }
+    $('#assign_series_events input[name="event_id"]').attr('value', event_id);
+    show_not_assigned_to_series_dialog();
+}
+
+function handleSchedule(id, start_date, event) {
+    const [, project_id, studio_id, series_id, event_id] = id.split('_');
+    if (checkStudio() == 0 || project_id < 0 || studio_id < 0 || series_id < 0) {
+        return;
+    }
+    if (event.which == 1) {
+        var params = new URLSearchParams({ action: "show_new_event_from_schedule", project_id, studio_id, series_id, start_date });
+        var url = "broadcast.cgi?" + params.toString();
+        loadUrl(url);
+    }
+    if (event.which == 3) {
+        var params = new URLSearchParams({ action: "show_series", project_id, studio_id, series_id, start: start_date, exclude: 1, show_hint_to_add_schedule: 1 });
+        var url = "series.cgi?" + params.toString() + '#tabs-schedule';
+        loadUrl(url);
+    }
+}
+
+function handleGrid(id) {
+    const [, project_id, studio_id, series_id, event_id] = id.split('_');
+    if (project_id < 0 || studio_id < 0) {
+        return;
+    }
+    const startTime = getNearestDatetime();
+    show_schedule_series_dialog(project_id, studio_id, series_id, startTime);
+}
+
+function handleWorktime(id, event) {
+    const [, project_id, studio_id, work_id] = id.split('_');
+    if (checkStudio() == 0 || project_id < 0 || work_id < 0) {
+        return;
+    }
+    var startDate = $('#' + id).attr("start");
+    var params = new URLSearchParams({ action: "show_new_event_from_schedule", project_id, studio_id, work_id, start_date: startDate });
+    var url = "work-time.cgi?" + params.toString();
+    if (event.which == 1) {
+        loadUrl(url);
+    }
+    if (event.which == 2) {
+        openNewTab(url);
+    }
+}
--- website/agenda/planung/js/list-events.js
+++ website/agenda/planung/js/list-events.js
@@ -23,9 +23,49 @@ function initSearch() {
     }
 }
 
+function update_urlParameters(url) {
+    if (url == null) {
+        url = window.location.href;
+    }
+    url = setUrlParameter(url, 'project_id', $('#project_id').val());
+    url = setUrlParameter(url, 'studio_id', $('#studio_id').val());
+    url = setUrlParameter(url, 's', isChecked('#show_schedule') ? 1 : 0);
+    url = setUrlParameter(url, 'e', isChecked('#show_events') ? 1 : 0);
+
+    return url;
+}
+
+function update_url(url) {
+    if (url == null) url = update_urlParameters();
+    const urlObj = new URL(url, window.location.origin);
+    const d = urlObj.searchParams.get("date");
+    history.pushState(null, null, url);
+    appendHistory(url, 'replace');
+}
+
 // init function
 window.calcms ??= {};
-window.calcms.init_list_events = function(el) {
+window.calcms.init_list_events = async function(el) {
+    await loadLocalization('calendar');
     initSearch();
+    let url = update_url();
+    setColors();
+    setup_actions();
+    document.querySelectorAll('table td.start_date').forEach(el => {
+        el.innerHTML = DTF.datetime(el.innerHTML);
+    });
+
+    document.querySelectorAll('table td.start_date').forEach(el => {
+        el.innerHTML = DTF.datetime(el.innerHTML);
+    });
+    $('#editSeries').attr('data-href',
+        "series.cgi?" + new URLSearchParams({
+            action: "show_series",
+            project_id: getUrlParameter('project_id'),
+            studio_id: getUrlParameter('studio_id'),
+            series_id: getUrlParameter('series_id'),
+        }).toString()
+    );
+    return;
 };
 
